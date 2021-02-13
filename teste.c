6660b77bc02488e81ee0e000ca97268f70191d5a
matheus2p <78156758+matheus2p@users.noreply.github.com>
2021-02-13 18:29:33 -0300

    Add files via upload

---
+++ teste.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <assert.h>
+#include <gc.h>
+
+int main(void)
+{
+    int i;
+    const int size = 10000000;
+
+    GC_INIT();
+    for (i = 0; i < size; ++i)
+    {
+        int **p = GC_MALLOC(sizeof *p);
+        int *q = GC_MALLOC_ATOMIC(sizeof *q);
+
+        assert(*p == 0);
+        *p = GC_REALLOC(q, 2 * sizeof *p);
+        if (i == size-1)
+            printf("Heap size = %zu\n", GC_get_heap_size());
+    }
+
+    return 0;
+}
